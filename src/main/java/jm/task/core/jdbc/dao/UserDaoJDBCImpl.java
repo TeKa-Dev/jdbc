8c6d44cb98273a6b98ba458c3af8ae8380e74a68
Teka-Dev <qasytqkn@gmail.com>
2022-06-16 18:02:28 +0500

    solution

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,7 +14,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {}
 
     public void createUsersTable() {
-        Util.offerQuery("create table users" +
+        offerQuery("create table users" +
                 "(id bigint not null auto_increment," +
                 "name varchar(15)," +
                 "last_name varchar(15)," +
@@ -22,24 +23,24 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        Util.offerQuery("drop table users");
+        offerQuery("drop table users");
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        Util.offerQuery("insert into users (name, last_name, age)" +
+        offerQuery("insert into users (name, last_name, age)" +
                 "values (\'" + name + "\', \'"+ lastName + "\', "+ age + ");");
     }
 
     public void removeUserById(long id) {
-        Util.offerQuery("delete from users where id = " + id);
+        offerQuery("delete from users where id = " + id);
     }
 
     public void cleanUsersTable() {
-        Util.offerQuery("delete from users");
+        offerQuery("delete from users");
     }
 
     public List<User> getAllUsers() {
-        try (ResultSet allSet = Util.getUsersSet()) {
+        try (ResultSet allSet = Util.getStatement().executeQuery("select * from users")) {
             List<User> allUsers = new ArrayList<>();
 
             while (allSet.next()) {
@@ -57,37 +58,11 @@ public class UserDaoJDBCImpl implements UserDao {
         }
     }
 
-
-    // Класс Util должен содержать логику настройки соединения с базой данных
-
-    static class Util {
-
-        private static Statement statement;
-
-        static {
-            try {
-                statement = DriverManager
-                        .getConnection("jdbc:mysql://localhost:3306/jdbc", "user", "user")
-                        .createStatement();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
         private static void offerQuery(String sql) {
             try {
-                statement.execute(sql);
+                Util.getStatement().execute(sql);
             } catch (SQLException e) {
                 // ignore
             }
         }
-
-        private static ResultSet getUsersSet() {
-            try {
-                return statement.executeQuery("select * from users");
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static Statement statement;
+
+    static {
+        try {statement = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/jdbc", "user", "user")
+                    .createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Statement getStatement() {
+        return statement;
+    }
 }
